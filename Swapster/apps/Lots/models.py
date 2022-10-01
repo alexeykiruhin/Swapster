e0ef9b10b704ffebe88e0909d224d7dfdbd3554e
alexeykiruhin <q23sa3501080@gmail.com>
2022-10-01 22:15:16 +0300

    добавлена связка лота и юзера, показываем лоты только свои, защита от просмотра чужих лотов по айди

--- Swapster/apps/Lots/models.py
+++ Swapster/apps/Lots/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Lot(models.Model):
+    user = models.CharField('user', max_length=50, null=True)
+    usernew = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     lot_title = models.CharField('Название', max_length=50)
     lot_text = models.CharField('Описание', max_length=200)
     lot_date = models.DateTimeField('Дата создания')
--- Swapster/apps/Lots/views.py
+++ Swapster/apps/Lots/views.py
@@ -4,11 +4,15 @@ from django.urls import reverse
 from django.utils import timezone
 
 from .models import Lot
+from django.contrib.auth.models import User
 
 
 def index(request):
-    latest_lots_list = Lot.objects.order_by('-lot_date')
-    return render(request, 'lots/list.html', {'latest_lots_list': latest_lots_list})
+    if request.user.is_anonymous:
+        return HttpResponseRedirect(reverse('swaps:index'))
+    else:
+        latest_lots_list = Lot.objects.filter(usernew_id=request.user.id)
+        return render(request, 'lots/list.html', {'latest_lots_list': latest_lots_list})
 
 
 def detail(request, lot_id):
@@ -16,12 +20,16 @@ def detail(request, lot_id):
         lot = Lot.objects.get( id = lot_id )
     except:
         raise Http404('Лот не найден')
-
-    return render(request, 'lots/detail.html', {'lot': lot})
+    # проверяем чей лот, если этого юзера то показываем
+    if lot.usernew_id == request.user.id:
+        return render(request, 'lots/detail.html', {'lot': lot})
+    else:
+        raise Http404('Лот не найден')
 
 
 def add_lot(request):
-    o = Lot(lot_title=request.POST['title'], lot_text=request.POST['text'], lot_date=timezone.now())
+    o = Lot(lot_title=request.POST['title'], lot_text=request.POST['text'], lot_date=timezone.now(), usernew_id=request.user.id)
+    print(request.user.id)
     o.save(force_insert=True)
     return HttpResponseRedirect(reverse('lots:index'))
 
